38ab636f66614e094c73a30ecffaeea8110172f5
ASWINGNAIR <aswingnair17@gmail.com>
2025-06-21 20:25:58 +0530

    Initial Commit 21-06-2025

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Components/Header'
+
+function LandingPage() {
+  return (
+    <>
+    <Header />
+        <div className='text-center text-danger'>Aswin</div>
+    </>
+  )
+}
+
+export default LandingPage
